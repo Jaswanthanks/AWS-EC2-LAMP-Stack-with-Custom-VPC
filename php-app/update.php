9449275d0762b02a6443efafe1d7d60fd26872b6
Jaswanthan KS <110350104+Jaswanthanks@users.noreply.github.com>
2025-08-11 19:51:12 +0530

    Update update.php

--- php-app/update.php
+++ php-app/update.php
@@ -1,23 +1,25 @@
 <?php
 include 'config.php';
 $id = $_GET['id'];
-$user = $conn->query("SELECT * FROM users WHERE id=$id")->fetch_assoc();
+$contact = $conn->query("SELECT * FROM contacts WHERE id=$id")->fetch_assoc();
 
 if ($_POST) {
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $conn->query("UPDATE users SET name='$name', email='$email' WHERE id=$id");
+    $message = $_POST['message'];
+    $conn->query("UPDATE contacts SET name='$name', email='$email', message='$message' WHERE id=$id");
     header("Location: index.php");
 }
 ?>
 <!DOCTYPE html>
 <html>
-<head><title>Edit User</title></head>
+<head><title>Edit Contact</title></head>
 <body>
-<h1>Edit User</h1>
+<h1>Edit Contact</h1>
 <form method="post">
-    Name: <input type="text" name="name" value="<?= $user['name'] ?>" required><br><br>
-    Email: <input type="email" name="email" value="<?= $user['email'] ?>" required><br><br>
+    Name: <input type="text" name="name" value="<?= $contact['name'] ?>" required><br><br>
+    Email: <input type="email" name="email" value="<?= $contact['email'] ?>" required><br><br>
+    Message: <textarea name="message"><?= $contact['message'] ?></textarea><br><br>
     <input type="submit" value="Update">
 </form>
 </body>
