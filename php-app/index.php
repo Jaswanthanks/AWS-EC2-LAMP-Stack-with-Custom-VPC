62a48350ba25696be62419819457006d55a2bcf0
Jaswanthan KS <110350104+Jaswanthanks@users.noreply.github.com>
2025-08-11 19:50:14 +0530

    Update index.php

--- php-app/index.php
+++ php-app/index.php
@@ -1,25 +1,29 @@
 <?php
 include 'config.php';
-$result = $conn->query("SELECT * FROM users");
+$result = $conn->query("SELECT * FROM contacts");
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>User List</title>
+    <title>Contact List</title>
 </head>
 <body>
-    <h1>Users</h1>
-    <a href="create.php">Add New User</a>
+    <h1>Contacts</h1>
+    <a href="create.php">Add New Contact</a>
     <table border="1" cellpadding="10">
-        <tr><th>ID</th><th>Name</th><th>Email</th><th>Actions</th></tr>
+        <tr>
+            <th>ID</th><th>Name</th><th>Email</th><th>Message</th><th>Created At</th><th>Actions</th>
+        </tr>
         <?php while($row = $result->fetch_assoc()) { ?>
         <tr>
-            <td><?php echo $row['id']; ?></td>
-            <td><?php echo $row['name']; ?></td>
-            <td><?php echo $row['email']; ?></td>
+            <td><?= $row['id'] ?></td>
+            <td><?= $row['name'] ?></td>
+            <td><?= $row['email'] ?></td>
+            <td><?= $row['message'] ?></td>
+            <td><?= $row['created_at'] ?></td>
             <td>
-                <a href="update.php?id=<?php echo $row['id']; ?>">Edit</a> |
-                <a href="delete.php?id=<?php echo $row['id']; ?>">Delete</a>
+                <a href="update.php?id=<?= $row['id'] ?>">Edit</a> |
+                <a href="delete.php?id=<?= $row['id'] ?>">Delete</a>
             </td>
         </tr>
         <?php } ?>
