3adb7b3765ac91721b9fa66104b32dbb099b7340
Jaswanthan KS <110350104+Jaswanthanks@users.noreply.github.com>
2025-08-11 19:45:04 +0530

    Update index.php

--- php-app/index.php
+++ php-app/index.php
@@ -14,12 +14,12 @@ $result = $conn->query("SELECT * FROM users");
         <tr><th>ID</th><th>Name</th><th>Email</th><th>Actions</th></tr>
         <?php while($row = $result->fetch_assoc()) { ?>
         <tr>
-            <td><?= $row['id'] ?></td>
-            <td><?= $row['name'] ?></td>
-            <td><?= $row['email'] ?></td>
+            <td><?php echo $row['id']; ?></td>
+            <td><?php echo $row['name']; ?></td>
+            <td><?php echo $row['email']; ?></td>
             <td>
-                <a href="update.php?id=<?= $row['id'] ?>">Edit</a> |
-                <a href="delete.php?id=<?= $row['id'] ?>">Delete</a>
+                <a href="update.php?id=<?php echo $row['id']; ?>">Edit</a> |
+                <a href="delete.php?id=<?php echo $row['id']; ?>">Delete</a>
             </td>
         </tr>
         <?php } ?>
